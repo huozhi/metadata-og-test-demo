3d355181d91917855a17dd6903823c10a9d80ea9
Jiachi Liu <inbox@huozhi.im>
2023-05-09 11:35:00 +0200

    dynamic routes with icon (#1)

---
+++ app/(group)/og/page.js
@@ -0,0 +1,14 @@
+export default () => {
+  return 'seg page'
+}
+
+export async function generateMetadata() {
+  return {
+
+    twitter: {
+      // card: 'summary_large_image',
+      title: 'seg title',
+      description: 'seg description',
+    }
+  }
+}
